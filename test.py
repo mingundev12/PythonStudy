03947590388fd10596dc00385d5d749b50f9041e
mingundev12 <mingundev12@gmail.com>
2026-04-21 12:21:50 +0900

    사각형 넓이

--- test.py
+++ test.py
@@ -8,3 +8,24 @@ print(name)
 job = "군인"
 print(name + job + " 이다")
 print("배고파" * 5)
+반지름 = 5
+pi = 3.14
+print(반지름 * 2 * pi)
+is_student = True
+is_food = False
+print(is_student)
+print(type(pi))
+# num = input("정수입력 : ")
+# num = int(num)
+# print(num + 10)
+# 한줄 주석 이다
+"""
+여러줄 주석
+"""
+
+#문제. 너비와 높이를 입력받아 사각형의 넓이를 출력하세요
+width = input("너비 입력 : ")
+height = input("높이 입력 : ")
+width = int(width)
+height = int(height)
+print("사각형의 넓이 : " + str(width * height))
