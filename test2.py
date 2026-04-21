84006e319893759b88d604ede5e86203d01cefe3
mingundev12 <mingundev12@gmail.com>
2026-04-21 12:42:25 +0900

    python에서의 특수한 연산자들

---
+++ test2.py
@@ -0,0 +1,15 @@
+#연산자 - 비교, 논리, 증감, 대입, 산술
+#산술연산자 / 나누기, %
+
+print(10 / 3) # 결과가 실수값
+print(10 // 3) # 몫을 구하는 연산
+print(10 % 3) # 나머지를 구하는 연산
+
+print(2 * 3) # 곱셈
+print(2 ** 3) # 거듭제곱
+
+num = 10
+
+print(num > 5 and 110 > 50)
+print(not num)
+print(not num > 5)
